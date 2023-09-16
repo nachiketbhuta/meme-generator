3e53ddc55bdec3c6606c3fb38923bb42b7593e39
Nachiket Bhuta <nachiketbhuta@gmail.com>
2023-09-16 23:46:08 +0530

    Initial commit

--- src/Main.test.tsx
+++ src/Main.test.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
-import App from './App';
+import Main from './Main';
 
 test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  render(<Main />);
+  const linkElement = screen.getByText(/Hi/i);
   expect(linkElement).toBeInTheDocument();
 });
